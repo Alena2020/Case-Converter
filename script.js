d1d6794c6aa05b09b165e6ae6ad8b1028b9a7e55
Alena <elenila_skinef@mail.ru>
2021-08-13 00:04:36 +0300

    added button Save Text File  and event listener for button, changed style app

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let buttonUpperCase = document.getElementById('upper-case');
 let buttonLowerCase = document.getElementById('lower-case');
 let buttonProperCase = document.getElementById('proper-case');
 let buttonSentenceCase = document.getElementById('sentence-case');
+let buttonSaveTextFile = document.getElementById('save-text-file');
 
 buttonUpperCase.addEventListener("click", () => {
     inputText.value = inputText.value.toUpperCase();
@@ -18,7 +19,7 @@ buttonLowerCase.addEventListener("click", () => {
 buttonProperCase.addEventListener("click", () => {
     textArray = inputText.value.toLowerCase().split(' ');
     for (let i = 0; i < textArray.length; i++) {
-        textArray[i] = textArray[i][0].toUpperCase() + textArray[i].slice(1);
+        textArray[i] = textArray[i].charAt(0).toUpperCase() + textArray[i].slice(1);
     }
     inputText.value = textArray.join(' ');
 });
@@ -26,8 +27,25 @@ buttonProperCase.addEventListener("click", () => {
 buttonSentenceCase.addEventListener("click", () => {
     textArray = inputText.value.toLowerCase().split('. ');
     for (let i = 0; i < textArray.length; i++) {
-        textArray[i] = textArray[i][0].toUpperCase() + textArray[i].slice(1);
+        textArray[i] = textArray[i].charAt(0).toUpperCase() + textArray[i].slice(1);
     }
-    inputText.value = textArray.join(". ");
+    inputText.value = textArray.join('. ');
 });
 
+buttonSaveTextFile.addEventListener("click", () => {
+    function download(filename, text) {
+        let element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+
+        element.style.display = 'none';
+        document.body.appendChild(element);
+
+        element.click();
+
+        document.body.removeChild(element);
+    }
+
+// Start file download.
+    download("text.txt", inputText.value);
+});
